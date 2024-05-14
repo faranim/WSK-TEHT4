9ec6b64bbcc05afc5a236543f1323d81a0d852d1
nimofarah <faranim01@gmail.com>
2024-05-15 01:16:29 +0300

    Assignment 2 done

---
+++ src/app.js
@@ -0,0 +1,28 @@
+import express from 'express';
+import api from './api/index.js';
+const app = express();
+
+app.use(express.json());
+app.use(express.urlencoded({extended: true}));
+
+app.use('/public', express.static('public'));
+
+app.use('/api/v1', api);
+
+app.get('/', (req, res) => {
+  res.send('Welcome to my REST API!');
+});
+
+app.get('/api/v1/cats', (req, res) => {
+  const cat = {
+    cat_id: 1,
+    name: 'Viski',
+    birthdate: '2001-12-04',
+    weight: 6,
+    owner: 'Nimo',
+    image: 'https://loremflickr.com/320/240/cat'
+  };
+  res.json(cat)
+});
+
+export default app;
